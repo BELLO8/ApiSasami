bdb2f94c07a382e8e68c33cafdbf9f6d792ea698
sie52 <58252050+sie52@users.noreply.github.com>
2022-04-11 19:42:08 +0000

    api

---
+++ app/Http/Resources/PersonneVulnerableResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PersonneVulnerableResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'nom'=>$this->nom,
+            'prenom'=>$this->prenom,
+            'adresse'=>$this->adrersse,
+            'tel'=>$this->tel,
+            'age'=>$this->age,
+        ];
+    }
+}
