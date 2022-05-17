f0508112fdcf8c3a141f3b368b84e17b14204383
ODA_44 <oda_44@iMac-de-ODA-44.local>
2022-05-17 21:21:17 +0000

    assigner

--- app/Http/Controllers/AssignationController.php
+++ app/Http/Controllers/AssignationController.php
@@ -35,6 +35,11 @@ class AssignationController extends Controller
     {
         $authTel = auth()->user()->telephone;
         $vulnerable = PersonneVulnerable::get()->where("telephone","=",$authTel);
+
+        if(empty($vulnerable)){
+            return response()->json(array('Message'=>"Vous n\'êtes pas une personne vulnerable"));
+        }
+
         foreach ($vulnerable as $vul){
             $id_vulnerable = $vul->id;
         }
@@ -48,19 +53,18 @@ class AssignationController extends Controller
             'reference' => 'required|exists:dispositif,reference',
         ], $messages = [
             'required' => ':attribute est un champ obligatoire.',
-            'exists' => 'Introuvable',
-            'unique'=> 'Déja assigner à une personne vulnerable'
-        ]);
-        if ($validate->fails()) {
-            return response()->json(['Erreur de validation' => $validate->errors()]);
-        }
+            'exists' => 'Ce dispositif n\'existe pas',
 
+        ]);
+        $errors = $validate->errors();
+        if ($validate->fails()) {
+            return response()->json(array('status' => "errors",'message' => $errors->first('reference')),400);
+        }
 
         $dispositif = Dispositif::where("reference","=",$request->reference)->get();
         foreach ($dispositif as $dispo){
             $disp = $dispo->id;
         }
-
 
          $input = [
             'freq_enrg' => 10,
@@ -75,13 +79,14 @@ class AssignationController extends Controller
             'required' => ':attribute est un champ obligatoire.',
             'unique'=> 'Déja assigner à une personne vulnerable'
         ]);
+        $errors = $validate->errors();
         if ($validate->fails()) {
-            return response()->json(['Erreur de validation' => $validate->errors()]);
+            return response()->json(array('status' => "info",'message' => $errors->first('id_dispositif')));
         }
         if (Assigner::create($input)) {
-            return response()->json(array('status' => 'true','Message' => "Assigner avec succès  merci!"), 200);
+            return response()->json(array('message' => "Assigner avec succès  merci!"), 200);
         } else {
-            return response()->json(array('status' => 'false','Message' => "Erreur d'assignation"));
+            return response()->json(array('message' => "Erreur d'assignation"));
         }
     }
     /**
@@ -113,33 +118,33 @@ class AssignationController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        $assigner = Assigner::find($id);
-        if (is_null($assigner)) {
-            return response()->json(array('status' => 'false','Message' => "Id introuvable"));
-        } else {
-            $input = $request->all();
-            $validate = Validator::make($input, [
-                'freq_enrg' => 'required|max:255',
-                'date' => 'required',
-                'id_personneV' => 'required|exists:personneVulnerable,id',
-                'id_dispositif' => 'required|exists:dispositif,id'
-            ], $messages = [
-                'required' => ':attribute est un champ obligatoire.',
-                'exists' => 'Introuvable'
-            ]);
+    // public function update(Request $request, $id)
+    // {
+    //     $assigner = Assigner::find($id);
+    //     if (is_null($assigner)) {
+    //         return response()->json(array('status' => 'false','Message' => "Id introuvable"));
+    //     } else {
+    //         $input = $request->all();
+    //         $validate = Validator::make($input, [
+    //             'freq_enrg' => 'required|max:255',
+    //             'date' => 'required',
+    //             'id_personneV' => 'required|exists:personneVulnerable,id',
+    //             'id_dispositif' => 'required|exists:dispositif,id'
+    //         ], $messages = [
+    //             'required' => ':attribute est un champ obligatoire.',
+    //             'exists' => 'Introuvable'
+    //         ]);
 
-            if ($validate->fails()) {
-                return response()->json(['Erreur de validation' => $validate->errors()]);
-            }
-            if ($assigner->update($input)) {
-                return response()->json(array('Message' => "Assignation renouvelée"));
-            } else {
-                return response()->json(array('Message' => "Erreur"));
-            }
-        }
-    }
+    //         if ($validate->fails()) {
+    //             return response()->json(['Erreur de validation' => $validate->errors()]);
+    //         }
+    //         if ($assigner->update($input)) {
+    //             return response()->json(array('Message' => "Assignation renouvelée"));
+    //         } else {
+    //             return response()->json(array('Message' => "Erreur"));
+    //         }
+    //     }
+    // }
 
     /**
      * Remove the specified resource from storage.
